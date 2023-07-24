cdacd9baaca8a9167e12c1d5d255be7da5bc3f30
fxfd24roman <140372071+fxfd24roman@users.noreply.github.com>
2023-07-24 08:34:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const pads = document.querySelectorAll(".pad");
+  w = window.innerWidth;
+  h = window.innerHeight;
+  // console.log(w);
+  // console.log(h);
+  if (w>h) {
+    a = h - 4;
+  }
+  else {
+    a = w - 4;
+  }
+  console.log(a);
+  // Функция для воспроизведения звука и создания капли
+  function playSoundWithDrop(soundURL, color) {
+    const audioPlayer = new Audio(soundURL);
+    audioPlayer.play();
+
+    const drop = document.createElement("div");
+    drop.classList.add("drop");
+    drop.style.borderColor = color;
+    drop.style.overflow = "hidden";
+    drop.style.width = a + "px";
+    drop.style.height = a + "px";
+    document.body.appendChild(drop);
+
+    setTimeout(() => {
+      drop.remove();
+    }, 500); // Увеличим время на 500 миллисекунд для корректного завершения анимации
+  }
+
+  // Обработчик событий для каждой кнопки пэда
+  pads.forEach((pad) => {
+    pad.addEventListener("click", () => {
+      const soundURL = pad.getAttribute("data-sound");
+      const color = pad.getAttribute("data-color");
+
+      playSoundWithDrop(soundURL, color);
+      pad.classList.toggle("active");
+    });
+  });
+});
